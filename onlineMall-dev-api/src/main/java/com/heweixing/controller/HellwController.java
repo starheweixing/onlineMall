335d0b0b405c1a5f1e2b77f50eb23eb363a3ff4a
heweixing <767209548@qq.com>
2021-05-07 22:09:17 +0800

    5.7提交

--- onlineMall-dev-api/src/main/java/com/heweixing/controller/HellwController.java
+++ onlineMall-dev-api/src/main/java/com/heweixing/controller/HellwController.java
@@ -1,6 +1,8 @@
 package com.heweixing.controller;
 
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
@@ -8,14 +10,23 @@ import springfox.documentation.annotations.ApiIgnore;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+
 //@Controller
 @ApiIgnore
 @RestController  //返回的都是json对象
 public class HellwController {
 
+    final static Logger logger = LoggerFactory.getLogger(HellwController.class);
+
     @GetMapping("/hello")
     public Object hello(){
-        return "helloWord";
+
+        logger.debug("debug: hello---");
+        logger.info("info: hello---");
+        logger.warn("warn: hello---");
+        logger.error("error: hello---");
+
+        return "hello Word";
     }
 
 
@@ -28,6 +39,9 @@ public class HellwController {
         httpSession.getAttribute("userInfo");
         return "ok";
     }
+
+
+
 
 
 }
--- onlineMall-dev-api/src/main/java/com/heweixing/controller/interceptor/UserTokenInterceptor.java
+++ onlineMall-dev-api/src/main/java/com/heweixing/controller/interceptor/UserTokenInterceptor.java
@@ -1,14 +1,29 @@
 package com.heweixing.controller.interceptor;
 
+import com.heweixing.utils.IMOOCJSONResult;
+import com.heweixing.utils.JsonUtils;
+import com.heweixing.utils.RedisOperator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class UserTokenInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private RedisOperator redisOperator;
+
+    public static final String REDIS_USER_TOKEN = "redis_user_token";
+
     /**
      * 拦截请求，在访问controller之前
+     *
      * @param request
      * @param response
      * @param handler
@@ -18,16 +33,57 @@ public class UserTokenInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        String userId = request.getHeader("headerUserId");
+        String userToken = request.getHeader("headerUserToken");
 
-        /**
-         * false请求被拦截,被驳回,验证出现了问题
-         * true:可以放行
-         */
-        return false;
+        if (StringUtils.isNoneBlank(userId) || StringUtils.isNoneBlank(userToken)) {
+            String uniqueToken = redisOperator.get(REDIS_USER_TOKEN + ":" + userId);
+            if (StringUtils.isBlank(uniqueToken)) {
+             //   System.out.println("请登录");
+                returnErrorResponse(response, IMOOCJSONResult.errorMsg("请登录....."));
+                return false;
+            } else {
+                if (!StringUtils.equals(userToken, uniqueToken)) {
+                //    System.out.println("账号可能在异地登陆");
+                    returnErrorResponse(response, IMOOCJSONResult.errorMsg("账号可能在异地登陆...."));
+                    return false;
+                }
+            }
+        } else {
+       //     System.out.println("请登录.....");
+            returnErrorResponse(response, IMOOCJSONResult.errorMsg("请登录....."));
+            return false;
+        }
+        return true;
     }
+
+    public void returnErrorResponse(HttpServletResponse response, IMOOCJSONResult result) {
+
+        OutputStream out = null;
+
+        try {
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("text/json");
+            out = response.getOutputStream();
+            out.write(JsonUtils.objectToJson(result).getBytes("utf-8"));
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(out != null){
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     /**
      * 请求访问controller之后，渲染视图之前
+     *
      * @param request
      * @param response
      * @param handler
@@ -41,6 +97,7 @@ public class UserTokenInterceptor implements HandlerInterceptor {
 
     /**
      * 请求访问contrller之后, 渲染视图之后
+     *
      * @param request
      * @param response
      * @param handler
